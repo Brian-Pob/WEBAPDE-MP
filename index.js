e5aad31b37aac8f83c8fb46b8158afbd38401ef6
Unknown <brian_poblete@dlsu.edu.ph>
2018-08-08 19:01:00 +0800

    Can signup

--- index.js
+++ index.js
@@ -14,9 +14,33 @@ mongoose.connect('mongodb://localhost:27017/memedb',{useNewUrlParser: true})
 server.use(express.static(__dirname + '/public'));
 server.set('view engine', 'ejs')
 
+const loginSchema = new mongoose.Schema({
+    user: {
+        type: String
+    },
+    pass: {
+        type: String
+    }
+}, {
+    versionKey: false
+})
+const loginModel = mongoose.model('users', loginSchema)
+
 server.get('/', function(req, resp){
     resp.render('./index')
 })
 
+server.post('/signup', function(req, resp){
+    const userInstance = loginModel({
+        user: req.body.inputUsernameSignup,
+        pass: req.body.inputPasswordSignup
+    })
+
+    userInstance.save(function(err, res){
+        if(err) return console.error(err)
+        else
+            resp.render('./profilepage')
+    })
+})
 const port = process.env.PORT | 9090
 server.listen(port)
