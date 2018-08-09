9915d2b10f51773fe497d4005f372eaf7d1a8e14
Brian Poblete <brian_poblete@dlsu.edu.ph>
2018-08-09 15:50:29 +0800

    Added Login, Signup, and Logout with sessions. Also basic upload

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 
 const server = express()
-server.use(express.urlencoded({extended: true}))
+server.use(express.urlencoded({
+    extended: true
+}))
 server.use(express.json())
 
 const bodyParser = require('body-parser')
-server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.urlencoded({
+    extended: true
+}))
 server.use(bodyParser.json())
 
 const crypto = require('crypto')
@@ -14,7 +18,9 @@ const session = require('express-session')
 const mongoStore = require('connect-mongo')(session)
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/memedb',{useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/memedb', {
+    useNewUrlParser: true
+})
 
 server.use(session({
     secret: 'memes',
@@ -22,7 +28,7 @@ server.use(session({
     saveUninitialized: true,
     store: new mongoStore({
         mongooseConnection: mongoose.connection,
-        ttl: 60*60,
+        ttl: 60 * 60,
         autoRemove: 'native'
     })
 }))
@@ -67,18 +73,23 @@ const userSchema = new mongoose.Schema({
 })
 const userModel = mongoose.model('users', userSchema)
 
-server.get('/', function(req, resp){
-    var emptyData = {
-        _id: 'empty',
-        user: 'empty',
-        pass: 'empty'
+server.get('/', function (req, resp) {
+    if (req.session.user === undefined) {
+        resp.render('./index')
+        
+    }else {
+        var searchQuery = {
+            user: req.session.user
+        }
+        userModel.findOne(searchQuery, function(req, user){
+            resp.render('./index', {
+                data: user
+            })
+        })
     }
-    resp.render('./index', {
-        data: emptyData
-    })
 })
 
-server.post('/signup', function(req, resp){
+server.post('/signup', function (req, resp) {
     var dt = dateTime.create();
     var dtFormat = dt.format('m/d/Y')
     var password = req.body.inputPasswordSignup
@@ -91,19 +102,19 @@ server.post('/signup', function(req, resp){
         posts: []
     })
 
-    userInstance.save(function(err, res){
-        if(err) return console.error(err)
-        else{
+    userInstance.save(function (err, res) {
+        if (err) return console.error(err)
+        else {
             req.session.user = userInstance.user
             resp.render('./index', {
                 data: userInstance
             })
         }
-            
+
     })
 })
 
-server.post('/login', function(req, resp){
+server.post('/login', function (req, resp) {
     var password = req.body.inputPasswordLogin
 
     const searchQuery = {
@@ -111,45 +122,86 @@ server.post('/login', function(req, resp){
         pass: crypto.createHash('md5').update(password).digest('hex')
     }
 
-    userModel.findOne(searchQuery, function(err, login){
-        if(err) return console.error(err)
-        if(login !== undefined && login._id !== null){
+    userModel.findOne(searchQuery, function (err, logindata) {
+        if (err) return console.error(err)
+        if (logindata !== undefined && logindata._id !== null) {
             // resp.redirect('/?login=success')
-            req.session.user = login.user
+            req.session.user = logindata.user
             console.log(req.session.user)
-            
+
             resp.render('./index', {
-                data: login
+                data: logindata
             })
-        }else{
+        } else {
             resp.redirect('/?login=failed')
         }
 
     })
 })
 
-server.get('/logout', function(req, resp){
-    var emptyData = {
-        _id: 'empty',
-        user: 'empty',
-        pass: 'empty'
-    }
-    resp.render('./index', {
-        data: emptyData
-    })
+server.get('/logout', function (req, resp) {
+    
+    // var emptyData = {
+    //     _id: 'empty',
+    //     user: 'empty',
+    //     pass: 'empty'
+    // }
+    req.session.user = null
+    resp.render('./index')
 })
 
-server.get('/visitprofile', function(req, resp){
+server.get('/visitprofile', function (req, resp) {
     var searchQuery = {
         user: req.session.user
     }
-    userModel.findOne(searchQuery, function(err, user){
+    userModel.findOne(searchQuery, function (err, user) {
         //get user posts
         //get user date joined
         //get user posts
     })
-    resp.render('./profilepage', {
-        data:data
+    resp.render('./profilepage')
+})
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: String // name of the person
+    },
+    title: {
+        type: String
+    },
+    image: {
+        type: String //url of the image
+    },
+    comments: {
+        type: [String] //array of comment IDs
+    },
+    tags: {
+        type: [String]
+    },
+    datePosted: {
+        type: String //date the post was created
+    }
+})
+
+const memeModel = mongoose.model('posts', postSchema)
+
+server.post('/createpost', function (req, resp) {
+    var dt = dateTime.create()
+    var dtFormat = dt.format('m/d/Y')
+    var tags = req.body.inputTags
+
+    const postInstance = memeModel({
+        user: req.session.user,
+        title: req.body.inputPostTitle,
+        image: req.body.inputPostLink,
+        comments: [],
+        tags: tags.split(','),
+        datePosted: dtFormat
+    })
+
+    postInstance.save(function (err, res) {
+        if (err) return console.error(err)
+        resp.render('/index')
     })
 })
 const port = process.env.PORT | 9090
